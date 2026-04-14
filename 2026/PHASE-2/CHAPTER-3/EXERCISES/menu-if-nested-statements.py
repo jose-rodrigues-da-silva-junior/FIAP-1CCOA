afac2fb7f402fcbfa455ab07454293ca3c35cc95
José Rodrigues <jose.rodrigues.emailpessoal@gmail.com>
2026-04-14 00:52:48 -0300

    cria a lógica do menu usando instruções condicionais simples

--- 2026/PHASE-2/CHAPTER-3/EXERCISES/menu-if-nested-statements.py
+++ 2026/PHASE-2/CHAPTER-3/EXERCISES/menu-if-nested-statements.py
@@ -12,3 +12,19 @@ print(f"""
 3 - EDIT USER
 4 - DEACTIVATE USER
 """)
+
+# variable to store the user's choice
+choice = int(input("Enter your choice: "))
+
+if choice == 0:
+    print("Exiting the program...\n")
+elif choice == 1:
+    print("You chose to register a user.\n")
+elif choice == 2:
+    print("You chose to consult a user.\n")
+elif choice == 3:           
+    print("You chose to edit a user.\n")
+elif choice == 4:
+    print("You chose to deactivate a user.\n")
+else:
+    print("Invalid choice. Please enter a number between 0 and 4.\n")
