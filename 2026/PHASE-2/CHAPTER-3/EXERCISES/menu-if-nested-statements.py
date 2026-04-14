8e0061a66fd3d0cbbe61483e3b760074dc1d3c1d
José Rodrigues <jose.rodrigues.emailpessoal@gmail.com>
2026-04-14 00:36:09 -0300

    cria um menu de opções para o usuário

--- 2026/PHASE-2/CHAPTER-3/EXERCISES/menu-if-nested-statements.py
+++ 2026/PHASE-2/CHAPTER-3/EXERCISES/menu-if-nested-statements.py
@@ -5,3 +5,10 @@ The user can choose an option by entering a number.
 The program should then display a message based on the user's choice.
 """
 
+print(f"""
+0 - EXIT
+1 - REGISTER USER
+2 - CONSULT USER
+3 - EDIT USER
+4 - DEACTIVATE USER
+""")
