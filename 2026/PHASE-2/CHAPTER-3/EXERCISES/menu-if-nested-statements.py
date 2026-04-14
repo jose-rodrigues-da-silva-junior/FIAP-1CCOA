11b9d5f810d9fd7e196f37fdcece29ce9142b29e
José Rodrigues <jose.rodrigues.emailpessoal@gmail.com>
2026-04-14 00:26:30 -0300

    cria o arquivo e adiciona um comentário sobre o exercicio

---
+++ 2026/PHASE-2/CHAPTER-3/EXERCISES/menu-if-nested-statements.py
@@ -0,0 +1,7 @@
+""" 
+Exercise 1: 
+Write a program that displays a menu of options to the user. 
+The user can choose an option by entering a number. 
+The program should then display a message based on the user's choice.
+"""
+
